9be5ec2f2cd77cd99e32cf5fd623b9ce0ff1ecb1
Monirul Islam <mimnets@gmail.com>
2026-02-22 10:44:06 +0600

    add terms and privacy page

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -13,9 +13,9 @@ function loadGoogleAnalytics() {
   document.head.appendChild(script);
 
   (window as any).dataLayer = (window as any).dataLayer || [];
-  function gtag(...args: any[]) { (window as any).dataLayer.push(args); }
-  gtag('js', new Date());
-  gtag('config', GA_ID);
+  (window as any).gtag = function(...args: any[]) { (window as any).dataLayer.push(arguments); };
+  (window as any).gtag('js', new Date());
+  (window as any).gtag('config', GA_ID);
 }
 
 const CookieConsent: React.FC = () => {
